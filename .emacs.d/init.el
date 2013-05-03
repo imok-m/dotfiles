38f8cc4376d783b577ff7dac822899595727ebe3
Masafumi Imaoka <masafumi.imaoka@gmail.com>
2013-05-03 11:35:37 +0900

    add installer settings

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -11,6 +11,18 @@
 ;; 引数のディレクトリとそのサブディレクトリを load-path に追加
 (add-to-load-path "elisp" "conf" "public-repos")
 
+;; add to package archives
+(add-to-list 'package-archives '("melpa" . "http://melpa.milkbox.net/packages/"))
+(add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/"))
+(add-to-list 'package-archives '("ELPA" . "http://tromey.com/elpa/"))
+
+;; init-loader
 ;; https://gist.github.com/zqwell/1021706/raw/b9aff51e7e40afa7abb3c7d6ef7708993ad93b04/init-loader.el
 (require 'init-loader)
 (init-loader-load "~/.emacs.d/conf")
+
+;; auto-install
+(when (require 'auto-install nil t)
+  (setq auto-install-directory "~/.emacs.d/elisp/")
+  (auto-install-update-emacswiki-package-name t)
+  (auto-install-compatibility-setup))
