a8f3964e732621574fe2a9c7e71d0525b2d5ec0b
imok-m <imok-m@users.noreply.github.com>
2015-12-14 15:30:00 +0900

    add js2-mode settings for jsx file format.

--- .emacs.d/conf/12-js2-mode.el
+++ .emacs.d/conf/12-js2-mode.el
@@ -1,5 +1,11 @@
 (autoload 'js2-mode "js2-mode" nil t)
-(add-to-list 'auto-mode-alist '("\\.js$" . js2-mode))
 (custom-set-variables '(js2-basic-offset 2))
+
+(add-to-list 'auto-mode-alist '("\\.js$" . js2-mode))
 (add-hook 'js2-mode-hook 'flycheck-mode)
 (add-hook 'js2-mode-hook 'highlight-symbol-mode)
+
+(add-to-list 'auto-mode-alist '("\\.jsx$" . js2-jsx-mode))
+(flycheck-add-mode 'javascript-eslint 'js2-jsx-mode)
+(add-hook 'js2-jsx-mode-hook 'flycheck-mode)
+(add-hook 'js2-jsx-mode-hook 'highlight-symbol-mode)
