1f181b0b77f0d0c71bf4336bff0a1bb9ce999171
imok-m <imok-m@users.noreply.github.com>
2017-02-24 15:35:31 +0900

    Update 14-rspec-mode.el

--- .emacs.d/conf/14-rspec-mode.el
+++ .emacs.d/conf/14-rspec-mode.el
@@ -3,5 +3,5 @@
   (ruby-mode) ; major-mode
   (rspec-mode) ; minor-mode
 )
-(add-to-list 'auto-mode-alist '("_spec\\.rb$" . rspec-mode))
+(add-to-list 'auto-mode-alist '("_spec\\.rb$" . rspec-and-ruby-mode))
 (custom-set-variables '(rspec-use-rake-flag nil))
